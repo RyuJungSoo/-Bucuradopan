feca509c838fd4c275ce7655abd2b00032d4031b
Hong <shyboy0113@gmail.com>
2024-02-03 01:05:20 +0900

    틀 제작 작업
    1. 인트로 화면 구현 + 튜토리얼 스킵 여부 구현 완료
    (아직 사운드 슬라이드 바로 값 조정하는 건 못 함)
    2. 튜토리얼 슬라이드 애니메이션 처럼 진행 후, 배틀 신으로 넘어가게끔 설정 완료

---
+++ Assets/Scenes/Intro/IntroSystem.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroSystem : MonoBehaviour
+{
+    public GameObject skipPanel;
+    public GameObject optionPanel;
+
+    private void Awake()
+    {
+        skipPanel.SetActive(false);
+        optionPanel.SetActive(false);
+    }
+
+    public void GameStart()
+    {
+        skipPanel.SetActive(true);
+    }
+
+    public void SkipTutorial(bool _bool)
+    {
+        if(_bool is true)
+        {
+            SceneManager.LoadScene("Battle");
+        }
+        else
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+    }
+
+    public void Option()
+    {
+        optionPanel.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
