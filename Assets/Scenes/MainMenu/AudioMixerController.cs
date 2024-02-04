c4d8f730d724071f6e3293489f6a6368eecd433c
Hong <shyboy0113@gmail.com>
2024-02-04 10:49:53 +0900

    게임 초반 옵션창에서 효과음 버튼 추가

--- Assets/Scenes/MainMenu/AudioMixerController.cs
+++ Assets/Scenes/MainMenu/AudioMixerController.cs
@@ -5,10 +5,20 @@ using UnityEngine.UI;
 public class AudioMixerController : MonoBehaviour
 {
     public AudioMixer audioMixer; // Reference to your Audio Mixer
+
+    //사운드 옵션에서 효과음 버튼
+    public AudioSource sfxTest;
+    
+
     public Slider masterVolumeSlider;
     public Slider bgmVolumeSlider;
     public Slider sfxVolumeSlider;
 
+    public void PlaySFXSound()
+    {
+        sfxTest.Play();
+    }
+    
     public void SetMasterVolume(float volume)
     {
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
