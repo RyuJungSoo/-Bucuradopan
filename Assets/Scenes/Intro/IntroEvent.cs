b738ebf6d2a8f1c2a94d27c1333bfafc82e4977c
Hong <shyboy0113@gmail.com>
2024-02-04 10:31:50 +0900

    마우스 클릭 jpg 중복 제거 + Intro부분에서 Scene넘어가는 부분 수정
    ChangeScene부분이 일반 함수라서 제대로 작동하지 않았음
    코루틴으로 수정

--- Assets/Scenes/Intro/IntroEvent.cs
+++ Assets/Scenes/Intro/IntroEvent.cs
@@ -3,33 +3,68 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
+//Fade In -> ShowImage -> Fade Out -> ChangeScene
+//위 순서로 코루틴이 진행됨.
+//ChangeScene을 일반 함수로 만들었더니 잘 작동하지 않았음
+
 public class IntroEvent : MonoBehaviour
 {
     public Image[] image;
 
     public Image fadePanel;
-    public float fadeDuration = 1.0f;
+    public float fadeDuration = 2.0f;
     private void Awake()
     {
         foreach(Image i in image)
         {
             i.gameObject.SetActive(false);
         }
-        
-        fadePanel.gameObject.SetActive(false);
+
+        image[0].gameObject.SetActive(true);
     }
     void Start()
     {
-        StartCoroutine(ShowImage());
+        StartCoroutine(FadeIn());
     }
-
-
-    public void ChangeScene()
-    {
-        SceneManager.LoadScene("Battle");
-    }
-    
     private IEnumerator FadeIn()
+    {
+        float startTime = 0f;
+
+        Color startColor = fadePanel.color;
+        Color targetColor = new Color(startColor.r, startColor.g, startColor.b, 0f);
+
+        while (startTime < fadeDuration)
+        {
+            startTime += Time.deltaTime;
+
+            float t = startTime / fadeDuration;
+            fadePanel.color = Color.Lerp(startColor, targetColor, t);
+            yield return null;
+        }
+
+        fadePanel.color = targetColor;
+
+        yield return ShowImage();
+
+    }
+    private IEnumerator ShowImage()
+    {
+
+        yield return new WaitForSeconds(2.0f);
+
+        foreach (Image i in image)
+        {
+            i.gameObject.SetActive(true);
+
+            yield return new WaitForSeconds(1.0f);
+
+            i.gameObject.SetActive(false);
+        }
+
+        yield return FadeOut();
+
+    }
+    private IEnumerator FadeOut()
     {
         float startTime = 0f;
         
@@ -47,22 +82,13 @@ public class IntroEvent : MonoBehaviour
 
         fadePanel.color = targetColor;
         
-        ChangeScene();
+        yield return ChangeScene();
     }
-    private IEnumerator ShowImage()
+    private IEnumerator ChangeScene()
     {
+        SceneManager.LoadScene("Battle");
 
-        foreach(Image i in image)
-        {
-            i.gameObject.SetActive(true);
-
-            yield return new WaitForSeconds(1.0f);
-
-            i.gameObject.SetActive(false);
-        }
-
-        yield return FadeIn();
-
+        yield return null;
     }
 
 }
